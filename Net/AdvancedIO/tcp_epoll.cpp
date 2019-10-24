b20432b747cd5576af62c972c549ea5787eb7fe3
Misaki <1761607418@qq.com>
2019-10-23 19:50:56 -0700

    2019.10.24

--- Net/AdvancedIO/tcp_epoll.cpp
+++ Net/AdvancedIO/tcp_epoll.cpp
@@ -1,12 +1,137 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sys/epoll.h>
+#include <unistd.h>
 #include "tcp_socket.hpp"
+#define MAX_EPOLL 1024
 class Epoll
 {
   public:
-    bool Add(TcpSocket& sock);
-    bool Del(TcpSocket& sock);
-    bool Wait(std::vector<TcpSocket> list);
+    Epoll()
+      :_epfd(-1)
+    {
+
+    }
+    ~Epoll()
+    {
+
+    }
+    bool Init()
+    {
+      _epfd = epoll_create(MAX_EPOLL);
+      if(_epfd < 0)
+      {
+        std::cerr << "create epoll error" << std::endl;
+        return false;
+      }
+      return true;
+    }
+    bool Add(TcpSocket& sock)
+    {
+      struct epoll_event ev;
+      int fd = sock.GetFd();
+      ev.events = EPOLLIN;
+      ev.data.fd = fd;
+      int ret = epoll_ctl(_epfd, EPOLL_CTL_ADD, fd, &ev);
+      if(ret < 0)
+      {
+        std::cerr << "append monitor error" << std::endl;
+        return false;
+      }
+      return true;
+    }
+    bool Del(TcpSocket& sock)
+    {
+      int fd = sock.GetFd();
+      int ret = epoll_ctl(_epfd, EPOLL_CTL_DEL, fd, NULL);
+      if(ret < 0)
+      {
+        std::cerr << "remove monitor error" << std::endl;
+        return false;
+      }
+      return true;
+    }
+    bool Wait(std::vector<TcpSocket>& list, int timeout = 3000)
+    {
+      struct epoll_event evs[MAX_EPOLL];
+      int nfds = epoll_wait(_epfd, evs, MAX_EPOLL, timeout);
+      if(nfds < 0)
+      {
+        std::cerr << "epoll monitor error" << std::endl;
+        return false;
+      }
+      else if(nfds == 0)
+      {
+        std::cerr << "epoll monitor timeout" << std::endl;
+        return false;
+      }
+      for(int i = 0; i < nfds; i++)
+      {
+        int fd = evs[i].data.fd;
+        TcpSocket sock;
+        sock.SetFd(fd);
+        list.push_back(sock);
+      }
+      return true;
+    }
   private:
-    int _epollfd;
+    int _epfd;
 };
+int main(int argc, char* argv[])
+{
+  if(argc != 3)
+  {
+    std::cout << "./tcp_epoll ip port" << std::endl;
+    return -1;
+  }
+  std::string srv_ip = argv[1];
+  uint16_t srv_port = atoi(argv[2]);
+  TcpSocket lst_sock;
+  CHECK_RET(lst_sock.Socket());
+  CHECK_RET(lst_sock.Bind(srv_ip, srv_port));
+  CHECK_RET(lst_sock.Listen());
+  Epoll e;
+  CHECK_RET(e.Init());
+  CHECK_RET(e.Add(lst_sock));
+  while(1)
+  {
+    std::vector<TcpSocket> list;
+    if(e.Wait(list) == false)
+    {
+      sleep(1);
+      continue;
+    }
+    for(size_t i = 0; i < list.size(); i++)
+    {
+      //监听套接字
+      if(list[i].GetFd() == lst_sock.GetFd())
+      {
+        TcpSocket cli_sock;
+        if(lst_sock.Accept(cli_sock) == false)
+        {
+          continue;
+        }
+        CHECK_RET(e.Add(cli_sock));
+      }
+      else 
+      {
+        std::string buf;
+        if(list[i].Recv(buf) == false)
+        {
+          list[i].Close();
+          continue;
+        }
+        std::cout << "Client Say:" << buf << std::endl;
+        buf.clear();
+        std::cin>> buf;
+        if(list[i].Send(buf) == false)
+        {
+          list[i].Close();
+          continue;
+        }
+      }
+    }
+  }
+  lst_sock.Close();
+}
