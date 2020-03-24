1bcef9dffa98dbdcc9319b29ef2c92736339a7df
Misaki <1761607418@qq.com>
2020-03-24 09:07:07 -0700

    2020.3.25

--- BaseIO/read_write.cpp
+++ BaseIO/read_write.cpp
@@ -1,4 +1,4 @@
-//read函数介绍
+//read和write函数介绍
 #include <iostream>
 #include <fcntl.h>
 #include <stdio.h>
@@ -12,11 +12,22 @@
  * buf:数据存放的目标缓冲区
  * nbyte:最多读取的数据长度，16位无符号整型，一次读取最多为65535个字节
  * Return Value:
- * 返回实际读取的文件长度，大部分情况下目标文件中有多少数据则读取多少数据并且返回读取长度；
+ * 返回实际读取的数据长度，大部分情况下目标文件中有多少数据则读取多少数据并且返回读取长度；
  * 如果是管道或者套接字目前暂无数据则会阻塞
  * 如果是普通文件，读到文件结尾返回0
  * 可以设置非阻塞读取，如果暂无数据则不会阻塞而回返回-1并将errno置为EAGAIN
  * 返回值ssize_t是一个带符号整形
+ * */
+/* ssize_t write(int fildes, const void *buf, size_t nbyte);
+ * 向文件中写入数据
+ * Argumentes:
+ * fildes:文件描述符
+ * buf:写入数据存放的缓冲区
+ * nbyte:写入的最长数据长度
+ * Return Value:
+ * 返回实际写入的数据长度
+ * */
+/* read和write参数和返回值都类似，但是对于不同的文件描述符例如pipe，socket等都有不同的返回方法或者异常处理方法
  * */
 int main()
 {
